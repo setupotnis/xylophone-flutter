882ba5e395024490d56b66f87e1dafca420ac383
Setu Potnis <setupotnis@gmail.com>
2020-03-11 20:44:27 -0400

    finished making app

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,12 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
-  Widget buildKey() {
+  Expanded buildKey({Color color, int soundNumber}) {
     return Expanded(
       child: FlatButton(
-        color: Colors.,
+        color: color,
         onPressed: () {
-          playSound(1);
+          playSound(soundNumber);
         },
       ),
     );
@@ -30,13 +30,13 @@ class XylophoneApp extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                buildKey(),
-                buildKey(),
-                buildKey(),
-                buildKey(),
-                buildKey(),
-                buildKey(),
-                buildKey(),
+                buildKey(color: Colors.red, soundNumber: 1),
+                buildKey(color: Colors.orange, soundNumber: 2),
+                buildKey(color: Colors.yellow, soundNumber: 3),
+                buildKey(color: Colors.green, soundNumber: 4),
+                buildKey(color: Colors.blue, soundNumber: 5),
+                buildKey(color: Colors.indigo, soundNumber: 6),
+                buildKey(color: Colors.purple, soundNumber: 7),
               ],
             ),
           ),
