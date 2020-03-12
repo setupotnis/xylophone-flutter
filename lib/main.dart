7ff667ae3369d834478323a119433e87a22e107d
Setu Potnis <setupotnis@gmail.com>
2020-03-11 20:02:38 -0400

    added 6 more buttons to make different sounds

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,62 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound() {
+    final player = AudioCache();
+    player.play('note1.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
           child: Container(
-            child: Center(
-              child: FlatButton(
-                onPressed: () {
-                  final player = AudioCache();
-                  player.play('note1.wav');
-                },
-                child: Text('Click Me'),
-              ),
+            child: Column(
+              children: <Widget>[
+                FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    playSound();
+                  },
+                ),
+                FlatButton(
+                  color: Colors.orange,
+                  onPressed: () {
+                    playSound();
+                  },
+                ),
+                FlatButton(
+                  color: Colors.yellow,
+                  onPressed: () {
+                    playSound();
+                  },
+                ),
+                FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    playSound();
+                  },
+                ),
+                FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    playSound();
+                  },
+                ),
+                FlatButton(
+                  color: Colors.indigo,
+                  onPressed: () {
+                    playSound();
+                  },
+                ),
+                FlatButton(
+                  color: Colors.purple,
+                  onPressed: () {
+                    playSound();
+                  },
+                ),
+              ],
             ),
           ),
         ),
